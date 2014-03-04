0a203b792e1e27cc9921162cd8584ab3217613e1
Mike Brown <mike.brown@excella.com>
2014-03-03 21:45:38 -0500

    Add text box to display topping info

--- healthy_salad_chooser/static/js/toppings.js
+++ healthy_salad_chooser/static/js/toppings.js
@@ -4,22 +4,31 @@ function refreshCounter() {
   $('#counter').text(selectedToppings);
 }
 
-function addToSelected(topping) {
+function addToSelected() {
     $(this).off('click').on('click', removeFromSelected);
     $('#toppingsSelected').append($(this));
     selectedToppings++;
     refreshCounter();
 }
 
-function removeFromSelected(topiing) {
+function removeFromSelected() {
     $(this).off('click').on('click', addToSelected);
     $('#toppingsChoices').append($(this));
     selectedToppings--;
     refreshCounter();
 }
 
+function hoverInfo() {
+    $(".ingredientInfo").load('/'+$(this).attr("id")+'/');
+}
+
+function noHoverInfo() {
+    $(".ingredientInfo").html('');
+}
+
 $(document).ready(function() {
   $( refreshCounter );
   $( "#toppingsChoices .topping" ).click(addToSelected);
+  $( "#toppingsChoices .topping" ).hover(hoverInfo, noHoverInfo);
 });
 
