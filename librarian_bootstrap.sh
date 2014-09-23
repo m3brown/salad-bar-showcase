e3857aa1912305aba65aca3bf0e1bfffcd0e0d5a
Mike Brown <mike.brown@excella.com>
2014-09-22 20:03:04 -0400

    Specify version of librarian-puppet, newer versions won't install

--- librarian_bootstrap.sh
+++ librarian_bootstrap.sh
@@ -2,7 +2,7 @@
 
 librarian_puppet_bin=`which librarian-puppet`
 if [ ! -x "$librarian_puppet_bin" ]; then
-  gem install librarian-puppet
+  gem install librarian-puppet -v 0.9.16
 fi
 
 cd /etc/puppet
